97adb9f891cdc7b5aad5f71273a0aca6f6b763b1
SaragadamVamsiKrishna <vamsikrishnas@streamlinedmedical.in>
2016-05-13 23:11:09 +0530

    node-schd

--- node-sched/public/js/app.js
+++ node-sched/public/js/app.js
@@ -506,7 +506,7 @@ jQuery(document).ready(function(){
     ajaxCall("getresources", {}, getresourcesAck);
     ajaxCall("getcounts", {}, populateWdayText);
 
-    var socketio = io.connect("http://localhost:8083");
+    var socketio = io.connect("http://49.206.64.209:8083");
     socketio.on('connect', function () {
         socketio.on('newAppointment', function(message) {
             var newDate = new Date(message.selecteddate);
