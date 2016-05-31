bcc87ff7cc081f3df9a6e62a58e1bb8d6f16a671
SaragadamVamsiKrishna <vamsikrishnas@streamlinedmedical.in>
2016-05-31 17:44:58 +0530

    node-socketio

--- node-socketio/server.js
+++ node-socketio/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var upload = require('jquery-file-upload-middleware');
 var app = express();
 var http = require('http').Server(app);
-var socketio = require('socket.io')(http);
+var socketio = require('socket.io');
 
 app.use('/uploadLogo', upload.fileHandler({
     uploadDir: __dirname + '/persistent-storage/uploads/logos',
@@ -24,28 +24,24 @@ app.use('/uploadService', upload.fileHandler({
 
 require('./routes/staticrouting.js')(app);
 
-socketio.on('connection', function (socket) {
+var server = http.listen(8083);
+
+socketio.listen(server).on('connection', function (socket) {
+    
     socket.on('room', function(room) {
+      socket.set('room', room, function() { console.log('room ' + room + ' saved'); } );
       socket.join(room);
-      socket.room = room;
     });
     
     socket.on('newAppointment', function (msg) {
-      socket.in(msg.subdomain).emit('newAppointment', msg);
+      socket.broadcast.to(msg.subdomain).emit('newAppointment', msg);
     });
 
     socket.on('updateAppointment', function (msg) {
-      socket.in(msg.subdomain).emit('updateAppointment', msg);
+      socket.broadcast.to(msg.subdomain).emit('updateAppointment', msg);
     });
 
     socket.on('disconnect', function(){
       socket.leave(socket.room);
     });
-});
-
-var server = http.listen(8083, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('images app listening at http://%s:%s', host, port);
 });
